ef5786438a2e7483e434ad7835170cbb8889e905
ByteAlex <bytealex@zerotwo.bot>
2021-02-16 21:35:33 +0100

    Started work on retry-strategies for rate-limit exceptions

--- pylon-gateway-client/src/main/java/lol/up/pylon/gateway/client/util/CompletableFutureStreamObserver.java
+++ pylon-gateway-client/src/main/java/lol/up/pylon/gateway/client/util/CompletableFutureStreamObserver.java
@@ -3,8 +3,12 @@ package lol.up.pylon.gateway.client.util;
 import io.grpc.stub.StreamObserver;
 import lol.up.pylon.gateway.client.event.EventContext;
 import lol.up.pylon.gateway.client.exception.GrpcException;
+import lol.up.pylon.gateway.client.exception.GrpcGatewayApiRateLimitedException;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 public class CompletableFutureStreamObserver<T> extends CompletableFuture<T> implements StreamObserver<T> {
 
@@ -13,21 +17,85 @@ public class CompletableFutureStreamObserver<T> extends CompletableFuture<T> imp
     private final ThreadLocal<EventContext> threadLocal;
     private final EventContext eventContext;
     private final GrpcException source;
+    private final ScheduledExecutorService scheduler;
+    private final Supplier<CompletableFutureStreamObserver<T>> retry;
+    private final int retryLimit;
+    private int retryIndex = 0;
 
     public CompletableFutureStreamObserver() {
-        threadLocal = EventContext.localContext();
-        eventContext = threadLocal.get();
+        this(null, null, new GrpcException("Source trace"));
+    }
+
+    public CompletableFutureStreamObserver(final ScheduledExecutorService scheduler,
+                                           final Supplier<CompletableFutureStreamObserver<T>> retry,
+                                           final GrpcException source) {
+        this.threadLocal = EventContext.localContext();
+        this.eventContext = threadLocal.get();
+        this.source = source;
+        this.scheduler = scheduler;
+        this.retry = retry;
+        this.retryLimit = 0;
+    }
+
+    public CompletableFutureStreamObserver(final ScheduledExecutorService scheduler,
+                                           final Supplier<CompletableFutureStreamObserver<T>> retry,
+                                           final int retryLimit) {
+        this.threadLocal = EventContext.localContext();
+        this.eventContext = threadLocal.get();
         this.source = new GrpcException("Source trace");
+        this.scheduler = scheduler;
+        this.retry = retry;
+        this.retryLimit = retryLimit;
     }
 
     @Override
     public void onNext(T entity) {
-        completeWithContext(() -> complete(entity));
+        completeWithContext(() -> {
+            complete(entity);
+            return true;
+        });
     }
 
     @Override
     public void onError(Throwable throwable) {
-        completeWithContext(() -> completeExceptionally(ExceptionUtil.asGrpcException(throwable, source)));
+        completeWithContext(() -> {
+            final GrpcException grpcException = ExceptionUtil.asGrpcException(throwable, source);
+            if (grpcException instanceof GrpcGatewayApiRateLimitedException) {
+                final GrpcGatewayApiRateLimitedException exception = (GrpcGatewayApiRateLimitedException) grpcException;
+                if (retry != null) {
+                    final int retryAt = exception.getApiError().getRateLimited().getRetryAt();
+                    if (retryAt > 0) {
+                        if (scheduler != null) {
+                            if (retryIndex++ < retryLimit) {
+                                scheduler.schedule(() -> {
+                                    retry.get().thenAccept(this::onNext)
+                                            .exceptionally(nestedException -> {
+                                                onError(nestedException);
+                                                return null;
+                                            });
+                                }, retryAt - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+                                return false;
+                            } else {
+                                return true;
+                            }
+                        }
+                    } else {
+                        if (retryIndex++ < retryLimit) {
+                            retry.get().thenAccept(this::onNext)
+                                    .exceptionally(nestedException -> {
+                                        onError(nestedException);
+                                        return null;
+                                    });
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+            }
+            completeExceptionally(grpcException);
+            return true;
+        });
     }
 
     @Override
@@ -37,11 +105,11 @@ public class CompletableFutureStreamObserver<T> extends CompletableFuture<T> imp
         }
     }
 
-    private void completeWithContext(final Runnable run) {
+    private void completeWithContext(final Supplier<Boolean> complete) {
         completed = true;
         final EventContext previous = threadLocal.get();
         threadLocal.set(this.eventContext);
-        run.run();
+        completed = complete.get();
         threadLocal.set(previous);
     }
 }
